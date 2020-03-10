2bf0e9992759f753534469c2c4f1fc6cb5406ad4
xiyezry <xiyezry@qq.com>
2020-03-10 21:55:24 +0800

    新增一个special add操作 以保存结果的最后两位

--- SingleCircleCPU/alu.v
+++ SingleCircleCPU/alu.v
@@ -18,12 +18,13 @@ module alu(A, B, Shift, ALUOp, C, Zero,Carry,Negative,Overflow,LastTwo);
 	reg Negative_temp;
 	reg Overflow_temp;
 	reg [31:0] C_temp;
+	reg [31:0] C_temp2;
 
 	assign Zero = Zero_temp;
 	assign Carry = Carry_temp;
 	assign Negative = Negative_temp;
 	assign Overflow = Overflow_temp;
-	assign C = C_temp;
+	assign C = C_temp2;
 	assign LastTwo = C_temp[1:0];
 	
 	wire [7:0] zero_flag;
@@ -34,93 +35,93 @@ module alu(A, B, Shift, ALUOp, C, Zero,Carry,Negative,Overflow,LastTwo);
        
 	always @( * ) begin
 	  case ( ALUOp )
-		  `ALU_NOP:  C_temp = A;                          
+		  `ALU_NOP:  C_temp2 = A;                          
 		  
 		  
 		  `ALU_ADD:  
 			begin
-			C_temp = A + B;
-			if( C_temp[31] == 1 )
+			C_temp2 = A + B;
+			if( C_temp2[31] == 1 )
 				Negative_temp = 1;
-			else if( C_temp[31] == 0 )
+			else if( C_temp2[31] == 0 )
 				Negative_temp = 0;
 			Carry_temp=0;
 			if ( A[31] & ~B[31]) Overflow_temp = 0; //A < 0, B >= 0, overflow = 0
 			else if ( ~A[31] & B[31] )  Overflow_temp = 0; // A >= 0, B < 0, overflow = 0
-			else if ( A[31] & B[31] & C_temp[31] ) Overflow_temp = 0; // A < 0, B < 0, C < 0, overflow = 0
-			else if ( ~A[31] & ~B[31] & ~C_temp[31]) Overflow_temp = 0; // A >= 0, B >= 0, C >= 0, overflow = 0
+			else if ( A[31] & B[31] & C_temp2[31] ) Overflow_temp = 0; // A < 0, B < 0, C < 0, overflow = 0
+			else if ( ~A[31] & ~B[31] & ~C_temp2[31]) Overflow_temp = 0; // A >= 0, B >= 0, C >= 0, overflow = 0
 			else begin
 				Overflow_temp = 1;
-				C_temp = 0;
+				C_temp2 = 0;
 				end
-			if( C_temp == 0 )
+			if( C_temp2 == 0 )
 				Zero_temp = 1;  
 			else 
 				Zero_temp = 0;
 			end
 		  `ALU_SUB:
 			begin
-			C_temp = A - B;      
+			C_temp2 = A - B;      
 			Carry_temp = 0;
 			if ( A[31] & B[31]) Overflow_temp = 0; //A < 0, B < 0, overflow = 0
 			else if ( ~A[31] & ~B[31] )  Overflow_temp= 0; //A >= 0, B >= 0, overflow = 0
-			else if ( A[31] & ~B[31] & C_temp[31] ) Overflow_temp = 0; //A < 0, B >= 0, R < 0, overflow = 0
-			else if ( ~A[31] & B[31] & ~C_temp[31] )  Overflow_temp = 0; //A >= 0, B < 0, R > 0, overflow = 0
+			else if ( A[31] & ~B[31] & C_temp2[31] ) Overflow_temp = 0; //A < 0, B >= 0, R < 0, overflow = 0
+			else if ( ~A[31] & B[31] & ~C_temp2[31] )  Overflow_temp = 0; //A >= 0, B < 0, R > 0, overflow = 0
 			else begin
 				Overflow_temp = 1;
-				C_temp = 0;
+				C_temp2 = 0;
 			end
-			if( C_temp == 0 )
+			if( C_temp2 == 0 )
 				Zero_temp = 1;
 			else 
 				Zero_temp = 0;
-			if( C_temp[31] == 1 )
+			if( C_temp2[31] == 1 )
 				Negative_temp = 1;
-			else if( C_temp[31] == 0 )
+			else if( C_temp2[31] == 0 )
 				Negative_temp = 0; 
 			end
 			
 		  `ALU_AND:
 			begin
-			C_temp = A & B;
-			if( C_temp == 0 )
+			C_temp2 = A & B;
+			if( C_temp2 == 0 )
                 Zero_temp = 1;
             else 
                 Zero_temp = 0;
             Carry_temp = 0;
             Overflow_temp = 0;
-            if( C_temp[31] == 1 )
+            if( C_temp2[31] == 1 )
                Negative_temp = 1;
-            else if( C_temp[31] == 0 )
+            else if( C_temp2[31] == 0 )
                Negative_temp = 0;
 		    end
 		  `ALU_OR:
 			begin
-			C_temp = A | B;
-			if( C_temp == 0 )
+			C_temp2 = A | B;
+			if( C_temp2 == 0 )
                 Zero_temp=1;
             else 
                 Zero_temp=0;
             Carry_temp=0;
             Overflow_temp=0;
-            if( C_temp[31] == 1 )
+            if( C_temp2[31] == 1 )
                 Negative_temp = 1;
-            else if( C_temp[31] == 0 )
+            else if( C_temp2[31] == 0 )
                 Negative_temp=0;
 			end  
 			
-		  `ALU_SLT:  C_temp = (A < B) ? 32'd1 : 32'd0;    // SLT/SLTI
+		  `ALU_SLT:  C_temp2 = (A < B) ? 32'd1 : 32'd0;    // SLT/SLTI
 		  
-		  `ALU_SLTU: C_temp = ({1'b0, A} < {1'b0, B}) ? 32'd1 : 32'd0;
+		  `ALU_SLTU: C_temp2 = ({1'b0, A} < {1'b0, B}) ? 32'd1 : 32'd0;
 		  
 		  `ALU_ADDU:
 			begin
-			C_temp = A + B;
-			if( C_temp == 0 )
+			C_temp2 = A + B;
+			if( C_temp2 == 0 )
 				Zero_temp = 1;
 			else
 				Zero_temp = 0;
-			if ( C_temp < A || C_temp < B ) Carry_temp = 1;
+			if ( C_temp2 < A || C_temp2 < B ) Carry_temp = 1;
 			else Carry_temp = 0;                 
 			Negative_temp = 0;
 			Overflow_temp = 0;
@@ -128,8 +129,8 @@ module alu(A, B, Shift, ALUOp, C, Zero,Carry,Negative,Overflow,LastTwo);
 			
 		  `ALU_SUBU:
 		    begin
-			C_temp = A - B;
-			if( C_temp == 0 )
+			C_temp2 = A - B;
+			if( C_temp2 == 0 )
 				Zero_temp = 1;
 			else 
 				Zero_temp = 0;
@@ -145,11 +146,11 @@ module alu(A, B, Shift, ALUOp, C, Zero,Carry,Negative,Overflow,LastTwo);
 		  //`ALU_SLL:
 		  `ALU_LUI:
 			begin 
-            C_temp = {B[15:0],16'b0};
-            if (C_temp == 0 )
+            C_temp2 = {B[15:0],16'b0};
+            if (C_temp2 == 0 )
                 Zero_temp = 1;
             else Zero_temp = 0;
-            if (C_temp[31])
+            if (C_temp2[31])
                 Negative_temp = 1;
             else Negative_temp = 0;
             Carry_temp = 0;
@@ -157,34 +158,56 @@ module alu(A, B, Shift, ALUOp, C, Zero,Carry,Negative,Overflow,LastTwo);
 			end
 		  `ALU_XOR:
 			begin 
-			C_temp = A^B;
-            if(C_temp==0)
+			C_temp2 = A^B;
+            if(C_temp2==0)
                 Zero_temp=1;
             else 
                 Zero_temp=0;
             Carry_temp=0;
             Overflow_temp=0;
-            if( C_temp[31] == 1 )
+            if( C_temp2[31] == 1 )
                Negative_temp = 1;
-            else if(C_temp[31] == 0 )
+            else if(C_temp2[31] == 0 )
                Negative_temp = 0;
 			end 
 		  `ALU_NOR:
 			begin
-			C_temp = ~(A|B);
-            if( C_temp == 0 )
+			C_temp2 = ~(A|B);
+            if( C_temp2 == 0 )
                 Zero_temp = 1;
             else 
                 Zero_temp = 0;
             Carry_temp=0;
             Overflow_temp=0;
-            if(C_temp[31]==1)
+            if(C_temp2[31]==1)
                 Negative_temp=1;
-            else if(C_temp[31]==0)
+            else if(C_temp2[31]==0)
 				Negative_temp=0;
 			end  
-		  
-		   default:   C_temp = A;                          // Undefined
+		  4'b1111:
+			begin
+				C_temp = A + B;
+				if( C_temp[31] == 1 )
+					Negative_temp = 1;
+				else if( C_temp[31] == 0 )
+					Negative_temp = 0;
+				Carry_temp=0;
+				if ( A[31] & ~B[31]) Overflow_temp = 0; //A < 0, B >= 0, overflow = 0
+				else if ( ~A[31] & B[31] )  Overflow_temp = 0; // A >= 0, B < 0, overflow = 0
+				else if ( A[31] & B[31] & C_temp[31] ) Overflow_temp = 0; // A < 0, B < 0, C < 0, overflow = 0
+				else if ( ~A[31] & ~B[31] & ~C_temp[31]) Overflow_temp = 0; // A >= 0, B >= 0, C >= 0, overflow = 0
+				else begin
+					Overflow_temp = 1;
+					C_temp = 0;
+					end
+				if( C_temp == 0 )
+					Zero_temp = 1;  
+				else 
+					Zero_temp = 0;
+				C_temp2 = {C_temp[31:2],2'b00};
+			end
+
+		   default:   C_temp2 = A;                          // Undefined
 	  endcase
 	end // end always
 
