b50fab225005eb4edfd19098c0101e54875699e8
xiyezry <xiyezry@qq.com>
2020-03-11 11:17:21 +0800

    删除不必要文件

--- SingleCircleCPU/work/instructionmemory/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity instructionmemory is
-    port(
-        IMAdd           : in     vl_logic_vector(6 downto 0);
-        Ins             : out    vl_logic_vector(31 downto 0)
-    );
-end instructionmemory;
