3600d0a4f25455a50e628b9a7b8fa13e6cf1f705
xiyezry <xiyezry@qq.com>
2020-03-11 11:07:50 +0800

    没啥用的修复

--- SingleCircleCPU/ctrl_encode_def.v
+++ SingleCircleCPU/ctrl_encode_def.v
@@ -6,18 +6,16 @@
 
 
 // ALU control signal
-`define ALU_NOP   4'b0000 
+`define ALU_NOP   4'b0000
 `define ALU_ADD   4'b0001
-`define ALU_SUB   4'b0010 
+`define ALU_SUB   4'b0010
 `define ALU_AND   4'b0011
 `define ALU_OR    4'b0100
 `define ALU_SLT   4'b0101
 `define ALU_SLTU  4'b0110
 `define ALU_ADDU  4'b0111
 `define ALU_SUBU  4'b1000
-
 `define ALU_LUI   4'b1100
 `define ALU_XOR   4'b1101
 `define ALU_NOR   4'b1110
-`define ALU_SpADD 4'b1111
 
