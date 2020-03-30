5c779d9df333169e40afec2a7907a006ad57a102
xiyezry <xiyezry@qq.com>
2020-03-30 22:33:20 +0800

    增加32位的四选一多路选择器

--- PoplineCPU/mux.v
+++ PoplineCPU/mux.v
@@ -73,6 +73,30 @@ module mux4 #(parameter WIDTH = 8)
         
 endmodule
 
+module mux4_32 #(parameter WIDTH = 32)
+             (d0, d1, d2, d3,
+              s, y);
+    
+    input  [WIDTH-1:0] d0, d1, d2, d3;
+    input  [1:0] s;
+    output [WIDTH-1:0] y;
+    
+    reg [WIDTH-1:0] y_r;
+    
+    always @( * ) begin
+        case ( s )
+            2'b00: y_r = d0;
+            2'b01: y_r = d1;
+            2'b10: y_r = d2;
+            2'b11: y_r = d3;
+            default: ;
+        endcase
+    end // end always
+    
+    assign y = y_r;
+        
+endmodule
+
 // mux8
 module mux8 #(parameter WIDTH = 8)
              (d0, d1, d2, d3,
