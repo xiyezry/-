e92009352b61974a569a0c8725f67225509ab19e
xiyezry <xiyezry@qq.com>
2020-03-30 22:32:18 +0800

    增加对RegisterRd、Rs、Rt信号的传输

--- PoplineCPU/id_exe.v
+++ PoplineCPU/id_exe.v
@@ -14,10 +14,16 @@ module id_exe(  input clk,
                 input id_ShiftDirection,
                 input id_ALUasrc,
                 input[31:0] id_EXTOUT,
+                input[4:0] id_RegisterRd,
+                input[4:0] id_RegisterRs,
+                input[4:0] id_RegisterRt,
                 output reg[31:0] exe_imm32,
                 output reg[31:0] exe_inst,
                 output reg[31:0] exe_RFRD1,
                 output reg[31:0] exe_RFRD2,
+                output reg[4:0] exe_RegisterRd,
+                output reg[4:0] exe_RegisterRs,
+                output reg[4:0] exe_RegisterRt,
                 output reg exe_RegDst,
                 output reg exe_MemRead,
                 output reg exe_MemtoReg,
@@ -36,7 +42,9 @@ module id_exe(  input clk,
             exe_inst <=32'h00000000;
             exe_RFRD1 <=32'h00000000;
             exe_RFRD2 <=32'h00000000;
-            exe_RegDst <=0;
+            exe_RegisterRd <=5'b00000;
+            exe_RegisterRs <=5'b00000;
+            exe_RegisterRt <=5'b00000;
             exe_MemRead <=0;
             exe_MemtoReg <=0;
             exe_ALUOp <=4'b0000;
@@ -53,7 +61,9 @@ module id_exe(  input clk,
             exe_inst <= id_inst;
             exe_RFRD1 <=id_RFRD1;
             exe_RFRD2 <= id_RFRD2;
-            exe_RegDst <= id_RegDst;
+            exe_RegisterRd <= id_RegisterRd;
+            exe_RegisterRs <= id_RegisterRs;
+            exe_RegisterRt <= id_RegisterRt;
             exe_MemRead <= id_MemRead;
             exe_MemtoReg <= id_MemtoReg;
             exe_ALUOp <= id_ALUOp;
