a292ba28ac97313e61d889dc2ddb4c587f8e6b14
xiyezry <xiyezry@qq.com>
2020-03-30 22:34:02 +0800

    将上升沿写入改为下降沿写入 以满足前半个时钟周期写入后半个时钟周期读取

--- PoplineCPU/RF.v
+++ PoplineCPU/RF.v
@@ -4,13 +4,15 @@
                input         RFWr,
                input  [4:0]  A1, A2, A3, 
                input  [31:0] WD, 
-               output [31:0] RD1, RD2);
+               output [31:0] RD1, 
+               output [31:0]RD2);
 
   reg [31:0] rf[31:0];
 
   integer i;
 
-  always @(posedge clk, posedge rst)
+  always @(negedge clk, posedge rst)
+  begin
     if (rst) begin    //  reset
       for (i=0; i<32; i=i+1)
         rf[i] <= 0; //  i;
@@ -28,6 +30,7 @@
             $display("r[%2d] = 0x%8X,", A3, WD);
           end
       end
+  end
   assign RD1 = (A1 != 0) ? rf[A1] : 0;
   assign RD2 = (A2 != 0) ? rf[A2] : 0;
 
