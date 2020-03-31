158a09715b7fde8b9873dffebb1861e25e626f96
xiyezry <xiyezry@qq.com>
2020-03-31 23:55:33 +0800

    增加对冒险监测单元的支持

--- PoplineCPU/PC.v
+++ PoplineCPU/PC.v
@@ -1,14 +1,15 @@
-module PC( clk, rst, NPC, PC );
+module PC( clk, rst,PCWrite, NPC, PC);
 
   input              clk;
   input              rst;
+  input              PCWrite;
   input       [31:0] NPC;
   output reg  [31:0] PC;
 
   always @(posedge clk, posedge rst)
     if (rst) 
       PC <= 32'h0000_0000;
-    else
+    else if(PCWrite==1)
       PC <= NPC;
       
 endmodule
